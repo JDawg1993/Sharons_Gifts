3bbf8925f0b5b56aef1e1ce5ec92529386115369
JDawg1993 <J0rdan.d3an1993@gmail.com>
2023-09-12 13:10:04 +0100

    Origin

---
+++ index.js
@@ -0,0 +1,5 @@
+function MusicHover(){
+	document.getElementById("Music").firstElementChild.firstElementChild.style.fontSize = "300%";}
+function MusicNormal(){
+	document.getElementById("Music").firstElementChild.firstElementChild.style.fontSize = "100%";}
+
